51713b74442456f96691f57a5b59e5b6aad1faf8
Mike Turquette <mturquette@ti.com>
2011-01-25 02:58:18 -0600

    OMAP4: introduce DPLL_ABE low power bypass/cascade
    DPLL_ABE can output 196.608MHz when fed from the 32KHz timer if
    CM_CLKMODE_DPLL_ABE.REGM4XEN is set.  This unique property allows us to make
    DPLL_ABE the parent of DPLL_CORE and then drive DPLL_MPU and DPLL_IVA, all
    of which are in Low Power Bypass mode.  This allows the system to remain
    usable while consuming far less power than when all of the DPLLs are locked.
    The primary use case for this mode low power audio playback with the display
    turned off.
    
    Many caveats exist in this DPLL cascading mode:
    DSS operation is unsupported, MPU is limited to 196.608MHz, ABE runs at
    49.152MHz in LP mode and there are no doubt others we haven't figured out
    yet.
    
    This patch partially achieves the DPLL cascading dream: currently DPLL_ABE
    is still fed from SYS_CK, but DPLL_CORE/IVA/MPU are appropriately bypassed.
    DPLL_USB is not handled specially but in normal operation (no USB plugged
    in) it is in Low Power Stop and doesn't cause any issue.  It is possible to
    bypass DPLL_PER but the functional clock frequencies are wrong, so the UARTs
    are junk, the MMC cannot read/write correctly and the I2c transfers are
    ruined.
    
    Thus the remaining work is to:
    1) get all the peripherals under DPLL_PER functioning correctly while
       bypassed
    2) move DPLL_ABE to the 32KHz clock so that SYS_CK can be gated during audio
       playback
    
    To enable this mode use the debugfs entry:
    echo 1 > /debug/pm_debug/dpll_cascade_enable
    
    To exit DPLL cascading mode:
    echo 0 > /debug/pm_debug/dpll_cascade_enable
    
    Change-Id: I243d7211d5bd1d98e6f3c4f140e6108db5db6d2d
    Signed-off-by: Mike Turquette <mturquette@ti.com>

--- arch/arm/mach-omap2/pm-debug.c
+++ arch/arm/mach-omap2/pm-debug.c
@@ -34,6 +34,8 @@
 #include <plat/voltage.h>
 #include <plat/dmtimer.h>
 
+#include <mach/omap4-common.h>
+
 #include "prm.h"
 #include "cm.h"
 #include "pm.h"
@@ -44,6 +46,8 @@ u32 sleep_while_idle;
 u32 wakeup_timer_seconds;
 u32 wakeup_timer_milliseconds;
 u32 omap4_device_off_counter = 0;
+int pmd_clks_enable;
+int dpll_cascade_global_state;
 
 #define DUMP_PRM_MOD_REG(mod, reg)    \
 	regs[reg_count].name = #mod "." #reg; \
@@ -662,6 +666,119 @@ static int option_set(void *data, u64 val)
 
 DEFINE_SIMPLE_ATTRIBUTE(pm_dbg_option_fops, option_get, option_set, "%llu\n");
 
+static int pmd_clks_set(void *data, u64 val)
+{
+	int *option = data;
+
+	if (option != &pmd_clks_enable)
+		return -EINVAL;
+
+	if(cpu_is_omap44xx()) {
+		struct clk *l3_instr_ick, *l3_main_3_ick, *ocp_wp_noc_ick;
+
+		l3_instr_ick = clk_get(NULL, "l3_instr_ick");
+		l3_main_3_ick = clk_get(NULL, "l3_main_3_ick");
+		ocp_wp_noc_ick = clk_get(NULL, "ocp_wp_noc_ick");
+
+		/* enable PMD clocks */
+		if (!*option && val) {
+			clk_enable(l3_instr_ick);
+			clk_enable(l3_main_3_ick);
+			clk_enable(ocp_wp_noc_ick);
+
+			*option = val;
+		/* disable PMD clocks */
+		} else if (*option && !val) {
+			clk_disable(l3_instr_ick);
+			clk_disable(l3_main_3_ick);
+			clk_disable(ocp_wp_noc_ick);
+
+			*option = val;
+		}
+	}
+
+	return 0;
+}
+
+static int pmd_clks_get(void *data, u64 *val)
+{
+	int *option = data;
+
+	if (option != &pmd_clks_enable)
+		return -EINVAL;
+
+	*val = *option;
+
+	return 0;
+}
+
+DEFINE_SIMPLE_ATTRIBUTE(pm_debug_pmd_clks_fops, pmd_clks_get, pmd_clks_set,
+		"%llu\n");
+
+static int __init omap4_pmd_clks_init(void)
+{
+	struct clk *l3_instr_ick, *l3_main_3_ick, *ocp_wp_noc_ick;
+
+	l3_instr_ick = clk_get(NULL, "l3_instr_ick");
+	l3_main_3_ick = clk_get(NULL, "l3_main_3_ick");
+	ocp_wp_noc_ick = clk_get(NULL, "ocp_wp_noc_ick");
+
+#ifdef CONFIG_OMAP4_PMD_CLKS_ENABLE
+	pmd_clks_enable = 1;
+
+	if (!(l3_instr_ick->usecount))
+		clk_enable(l3_instr_ick);
+	if (!(l3_main_3_ick->usecount))
+		clk_enable(l3_main_3_ick);
+	if (!(ocp_wp_noc_ick->usecount))
+		clk_enable(ocp_wp_noc_ick);
+#else
+	pmd_clks_enable = 0;
+
+	if (l3_instr_ick->usecount)
+		clk_disable(l3_instr_ick);
+	if (l3_main_3_ick->usecount)
+		clk_disable(l3_main_3_ick);
+	if (ocp_wp_noc_ick->usecount)
+		clk_disable(ocp_wp_noc_ick);
+#endif
+
+	return 0;
+}
+
+static int dpll_cascading_set(void *data, u64 val)
+{
+	int *option = data;
+
+	if (option != &dpll_cascade_global_state)
+		return -EINVAL;
+
+	if (*option == 0 && val == 1) {
+		*option = val;
+		omap4_dpll_low_power_cascade_enter();
+	} else if (*option == 1 && val == 0) {
+		omap4_dpll_low_power_cascade_exit();
+		*option = val;
+	}
+
+	return 0;
+}
+
+static int dpll_cascading_get(void *data, u64 *val)
+{
+	int *option = data;
+
+	if (option != &dpll_cascade_global_state)
+		return -EINVAL;
+
+	*val = *option;
+
+	return 0;
+}
+
+DEFINE_SIMPLE_ATTRIBUTE(pm_debug_dpll_cascading_fops, dpll_cascading_get,
+		dpll_cascading_set, "%llu\n");
+
 static int __init pm_dbg_init(void)
 {
 	int i;
@@ -715,6 +832,17 @@ static int __init pm_dbg_init(void)
 			&pm_dbg_option_fops);
 	(void) debugfs_create_file("enable_sr_vp_debug",  S_IRUGO | S_IWUSR, d,
 				   &enable_sr_vp_debug, &pm_dbg_option_fops);
+
+	if (cpu_is_omap44xx()) {
+		omap4_pmd_clks_init();
+		debugfs_create_file("pmd_clks_enable", S_IRUGO|S_IWUGO, d,
+				&pmd_clks_enable, &pm_debug_pmd_clks_fops);
+
+		debugfs_create_file("dpll_cascade_enable", S_IRUGO|S_IWUGO, d,
+				&dpll_cascade_global_state,
+				&pm_debug_dpll_cascading_fops);
+	}
+
 	pm_dbg_main_dir = d;
 	pm_dbg_init_done = 1;
 
