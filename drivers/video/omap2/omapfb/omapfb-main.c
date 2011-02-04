1a2750391a9a4c3a09422c29d7d7a2e222cd9517
Axel Haslam <axelhaslam@ti.com>
2011-02-04 13:41:43 -0600

    KW: OMAPFB: protect againt null pointer dereference.
    Display pointer may be null. Do not dereffernece
    a null pointer.
    
    Change-Id: I18cd108b786eb4a6e7f285ad14c53cea6ccb9108
    Signed-off-by: Axel Haslam <axelhaslam@ti.com>

--- drivers/video/omap2/omapfb/omapfb-main.c
+++ drivers/video/omap2/omapfb/omapfb-main.c
@@ -719,13 +719,7 @@ int check_fb_var(struct fb_info *fbi, struct fb_var_screeninfo *var)
 			var->xres, var->yres,
 			var->xres_virtual, var->yres_virtual);
 
-	
-	if(display == NULL) {
-		return -EINVAL;
-	}
-
-	
-	if (display && display->driver->get_dimension) {
+	if (display && display->driver->get_dimension)
 		display->driver->get_dimension(display, &var->width, &var->height);
 	} else {
 		var->height = -1;
