c6478473bc4dba040dd6279cb0382fa8779cb158
Madhusudhan Chikkature <madhu.cr@ti.com>
2011-02-22 11:00:50 -0600

    OMAP4:Resolve memleak in CPU1 hotplug scenario
    The freq table is allocated every time CPU1 is initialized.
    Free the table in exit path to resolve the memleak.
    
    Change-Id: I38a171f0d9a0c9e623b04883c68bdd79229bc74f
    Signed-off-by: Madhusudhan Chikkature <madhu.cr@ti.com>

--- arch/arm/plat-omap/include/plat/opp.h
+++ arch/arm/plat-omap/include/plat/opp.h
@@ -100,6 +100,7 @@ u8 __deprecated opp_get_opp_id(struct omap_opp *opp);
 
 void opp_init_cpufreq_table(struct device *dev,
 			    struct cpufreq_frequency_table **table);
+void opp_exit_cpufreq_table(struct cpufreq_frequency_table **table);
 
 struct device **opp_init_voltage_params(struct voltagedomain *voltdm,
 					int *dev_count);
@@ -192,6 +193,11 @@ static inline u8 __deprecated opp_get_opp_id(struct omap_opp *opp)
 static inline
 void opp_init_cpufreq_table(struct device *dev,
 			    struct cpufreq_frequency_table **table)
+{
+}
+
+static inline
+void opp_exit_cpufreq_table(struct cpufreq_frequency_table **table)
 {
 }
 
