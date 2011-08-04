f1292b61f12fc1310dd8804755176b2c9ee6a714
Ricardo Cerqueira <github@cerqueira.org>
2011-08-04 23:11:53 +0100

    ASoC: cosmo: Add vaux2 regulator for AV Switch
    Originally Change-Id Ibb0264926ca599e8eae99ef0b747e8e08a4e316b

--- arch/arm/mach-omap2/board-cosmopolitan.c
+++ arch/arm/mach-omap2/board-cosmopolitan.c
@@ -598,6 +598,10 @@ static struct regulator_init_data sdp4430_vaux1 = {
 	},
 };
 
+static struct regulator_consumer_supply sdp4430_vaux2_supply[] = {
+	REGULATOR_SUPPLY("av-switch", "soc-audio"),
+};
+
 static struct regulator_init_data sdp4430_vaux2 = {
 	.constraints = {
 		.min_uV			= 2800000,
@@ -613,6 +617,8 @@ static struct regulator_init_data sdp4430_vaux2 = {
 			.disabled       = true,
 		},
 	},
+	.num_consumer_supplies  = 1,
+	.consumer_supplies      = sdp4430_vaux2_supply,
 };
 
 static struct regulator_init_data sdp4430_vaux3 = {
